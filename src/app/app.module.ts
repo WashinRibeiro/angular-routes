4c01a1cda3cfc0b7a355c5f7c510a9568903eea9
Washington Ribeiro <washinuchiha@gmail.com>
2022-12-07 17:31:26 -0300

    feat: creation of the title and card components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexModule } from './pages/index/index.module';
+import { PortifolioModule } from './pages/portifolio/portifolio.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IndexModule,
+    PortifolioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
